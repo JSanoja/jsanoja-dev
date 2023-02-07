ab16bfc2bb67a99c42c7538d7293aded6076f037
Juan Sanoja <juan.sanoja@gmail.com>
2023-02-07 17:23:33 -0300

    deploys

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+echo "Making public dir"
 mkdir public
+echo "Enter folder project"
 cd client
+echo "Install dependencies"
 npm i
+echo "Run build project"
 npm run build
+echo "Moving client to public dir"
 mv ./dist/client/* ../public/
-echo "Finish deploy on jsanoja.dev!"
+echo "Finish deploy /public/ on jsanoja.dev!"
